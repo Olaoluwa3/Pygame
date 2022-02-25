7a7ba506a4913ec0e980e81332cfb8ea50b583b2
Olaoluwa3 <olaoko3@gmail.com>
2022-02-24 22:08:37 -0600

    Update program with Settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,14 @@
 import sys
 from turtle import bgcolor
 import pygame
+from settings import Settings
 
 def run_game():
-    #Initialize game and create a screen object.
+    #Initialize game, settings, and screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 600))
+    ai_settings = Settings()
+
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     #Set the background color.
@@ -21,7 +24,7 @@ def run_game():
 
         
         #Redraw the screen during each pass through the loop.
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         
         #Make the most recently drawn screen visible.
         pygame.display.flip()
