3c0d7ce9fb4215606f6529273e6cec037068da89
Olaoluwa3 <olaoko3@gmail.com>
2022-02-26 21:13:53 -0600

    Resetting game

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,7 +35,7 @@ def run_game():
     #Start the main loop for the game.
     while True:
 
-        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)  #Watch for keyboard and mouse events.
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)  #Watch for keyboard and mouse events.
 
         if stats.game_active:
             ship.update()
