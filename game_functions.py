023ff2b5f680246442734079d3ccd993cdabd139
Olaoluwa3 <olaoko3@gmail.com>
2022-02-26 21:29:35 -0600

    Leveling Up

--- game_functions.py
+++ game_functions.py
@@ -51,6 +51,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """Start a new game when the player clicks Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        #Reset the game settings
+        ai_settings.initialize_dynamic_settings()
+
         #Hide the mouse cursor.
         pygame.mouse.set_visible(False)
 
@@ -108,8 +111,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
 
 
     if len(aliens) == 0:
-        #Destroy existing bullets and create new fleet.
+        #Destroy existing bullets, speed up game, and create new fleet.
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
@@ -179,9 +183,6 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
 
     #Look for aliens hitting the bottom of the screen
     check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
-
-
-
 
 
 def check_fleet_edges(ai_settings, aliens):
