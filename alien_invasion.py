abbf7834ec2fec7813ed961eba78e1326d6356b3
Olaoluwa3 <olaoko3@gmail.com>
2022-02-26 20:17:46 -0600

    Game over when ship runs out

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,9 +32,12 @@ def run_game():
     while True:
 
         gf.check_events(ai_settings, screen, ship, bullets)  #Watch for keyboard and mouse events.
-        ship.update ()
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
                 
