fea443838de2d018d5292e22c3832b02b390c73e
Olaoluwa3 <olaoko3@gmail.com>
2022-02-26 18:14:11 -0600

    Respond to alien-ship collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@
 import pygame
 from pygame.sprite import Group
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -13,10 +14,15 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    #Create an instance to store game statistics.
+    stats = GameStats(ai_settings)
+
     #Make a ship, a group of bullets, and a group of aliens. 
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
+
+
 
     #Create the fleet of aliens
     gf.create_fleet(ai_settings, screen, ship, aliens)
@@ -28,7 +34,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)  #Watch for keyboard and mouse events.
         ship.update()
         gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, aliens)
+        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
                 
