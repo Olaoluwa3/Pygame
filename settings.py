5c746348d1686d6374bc975ef6c8d85ed51a8805
Olaoluwa3 <olaoko3@gmail.com>
2022-02-28 01:33:30 -0600

    Rounding the score

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ class Settings():
         """Initialize settings that change throughout the game."""
         self.ship_speed_factor = 1.5
         self.bullet_speed_factor = 3
-        self.alien_speed_factor = 1
+        self.alien_speed_factor = 0.2
 
         #fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
@@ -49,5 +49,5 @@ class Settings():
         self.alien_speed_factor *= self.speedup_scale
 
         self.alien_points = int(self.alien_points * self.score_scale)
-        print(self.alien_points)
+        
 
